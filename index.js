0abae67196b50a375fd6d6ad8a77896418ea89f3
son189 <son.august.7334@gmail.com>
2022-11-28 09:11:48 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Mercy", "Jane", "SUzzy"];
+const writeCards = function(names, event){
+    let gifts  = [];
+    for (let t = 0; t < names.length; t++){
+        let gifts = `Thank you, ${names[t]}, for the wonderful ${event} gift!`
+        gifts.push(gifts)
+    }
+
+    return gifts;
+}
+writeCards(names, "greetings")
+
+const presents = function(k){
+    let t = k;
+    while(t >= 0){
+        console.log(t);
+        t--
+    }
+}
+
+presents (6);
